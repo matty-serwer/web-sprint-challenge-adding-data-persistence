81584f70a7cdbe3b9045219df8d80a16fca2a5e0
Matthew Serwer <Matthew.Serwer@gmail.com>
2020-12-11 13:45:32 -0500

    MVP endpoints for resource router functionaing. / GET and / POST.

--- api/resource/model.js
+++ api/resource/model.js
@@ -1,1 +1,14 @@
-// build your `Resource` model here
+const db = require('./../../data/dbConfig');
+
+module.exports = {
+  get() {
+    return db('resources');
+  },
+  insert(resource) {
+    return db('resources')
+      .insert(resource)
+      .then(([id]) => {
+        return db('resources').where('resource_id', id);
+      });
+  },
+};
--- api/resource/router.js
+++ api/resource/router.js
@@ -6,4 +6,39 @@ const Resource = require('./model');
 // adding a new resource - [POST] /api/resources
 // retrieving all resources - [GET] /api/resources
 
+// middlewares
+const validateResource =  (req, res, next) => {
+  if (!req.body) {
+    res.status(400).json({ message: "Missing resource data" });
+  } else if (!req.body.name) {
+    res
+      .status(400)
+      .json({ message: "Resource name is required" });
+  } else {
+    next();
+  }
+}
+
+// endpoints
+
+router.get('/', (req, res) => {
+  Resource.get()
+    .then(resources => {
+      res.json(resources);
+    })
+    .catch(e => {
+      res.status(500).json({ message: e.message });
+    })
+})
+
+router.post('/', validateResource, (req, res) => {
+  Resource.insert(req.body)
+    .then(resource => {
+      res.json(resource);
+    })
+    .catch(e => {
+      res.status(500).json({ message: e.message });
+    })
+})
+
 module.exports = router;
