943f69b06f51e776db1c62962195305264af1b5b
Matthew Serwer <Matthew.Serwer@gmail.com>
2020-12-11 11:44:56 -0500

    set up db config

--- data/dbConfig.js
+++ data/dbConfig.js
@@ -1,1 +1,7 @@
-// complete your db configuration
+const knex = require('knex');
+
+const knexConfig = require('../knexfile.js');
+
+const environment = process.env.NODE_ENV || "development";
+
+module.exports = knex(knexConfig[environment]);
